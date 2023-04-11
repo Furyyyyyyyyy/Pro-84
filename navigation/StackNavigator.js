61f2c592ea841c2eb8201f0275815e6613078156
Divyam <129356833+Furyyyyyyyyy@users.noreply.github.com>
2023-04-11 17:49:18 +0530

    Add files via upload

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import TabNavigator from "./TabNavigator";
-import StoryScreen from "../screens/StoryScreen";
+import PostScreen from "../screens/PostScreen";
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName="Tab"
-      screenOptions={{
-        headerShown: false
-      }}
-    >
-      <Stack.Screen name="Tab" component={TabNavigator} />
-      <Stack.Screen name="StoryScreen" component={StoryScreen} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator
+            initialRouteName="stack"
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name="stack" component={TabNavigator} />
+            <Stack.Screen name="PostScreen" component={PostScreen} />
+        </Stack.Navigator>
+    );
 };
 
 export default StackNavigator;
